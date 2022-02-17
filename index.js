0a189d18eae8e0d25efe6e8fe6d2baf799dcfb6b
Rythmere <rythmere@gmail.com>
2022-02-17 14:31:00 -0600

    added GET request for users

--- index.js
+++ index.js
@@ -67,6 +67,15 @@ app.get('/directors/:name', passport.authenticate('jwt', {session: false}), (req
   });
 });
 
+app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res)=> {
+  Users.findOne({ Username: req.params.Username}).then((user)=> {
+    res.json(user);
+  }).catch((err)=> {
+    console.error(err);
+    res.status(500).send('Error'+ err);
+  })
+});
+
 app.post('/users', [
   check('Username', 'Username is required').not().isEmpty(),
   check('Username', 'Username contains non alphanumeric characters').isAlphanumeric(),
